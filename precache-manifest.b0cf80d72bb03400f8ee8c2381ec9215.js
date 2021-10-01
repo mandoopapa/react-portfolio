dc4570f0d9b93614b51ae3e2e65c87e4627d6cc7
mandoopapa <remonytea@gmail.com>
2021-10-01 15:48:37 +0900

    Updates

---
+++ precache-manifest.b0cf80d72bb03400f8ee8c2381ec9215.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa472ae1b9440e4f9ca7f7c400d7e88",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "407ebc06a16d23572b83",
+    "url": "/react-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "af88727e85ba5e3a7030",
+    "url": "/react-portfolio/static/css/main.0abb0a98.chunk.css"
+  },
+  {
+    "revision": "407ebc06a16d23572b83",
+    "url": "/react-portfolio/static/js/2.dc392539.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-portfolio/static/js/2.dc392539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af88727e85ba5e3a7030",
+    "url": "/react-portfolio/static/js/main.dcedb309.chunk.js"
+  },
+  {
+    "revision": "a05a49d9351ba3961537",
+    "url": "/react-portfolio/static/js/runtime-main.bc59954b.js"
+  }
+]);
